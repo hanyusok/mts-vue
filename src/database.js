1af8ab0c7a40b0ab50042e334dce2e26ac7be4f7
hanyusok <hanyusok@gmail.com>
2023-06-13 18:04:30 +0900

    saledb test

--- src/database.js
+++ src/database.js
@@ -3,9 +3,9 @@
 const dbOptions = {
     host: 'localhost',
     port: 3050,
-    database: 'C:\\Mts3\\Db\\MTSDB.FDB',
-    user: 'MTSUSER',
-    password: 'mts3',
+    database: '../../SALESDB.FDB',
+    user: 'sysdba',
+    password: 'masterkey',
     lowercase_keys: false, // set to true to lowercase keys
     role: null,            // default
     pageSize: 4096,        // default when creating database
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { dbOptions } from '../database'
-// import firebird from 'node-firebird'
+import firebird from 'node-firebird'
 
-console.log('test')
+console.log('is starting')
 
 /* const tryConnect = () => {
   firebird.attach(dbOptions, (err, db) => {
@@ -27,6 +27,89 @@ console.log('test')
 } */
 
 
+const firebirdPool = firebird.pool(5, dbOptions);
+  
+const  NewTransaction = () => {
+  return new Promise((resolve, reject) => {      
+    firebirdPool.get((err, db) => {
+      if (err) { 
+        reject(err)
+        return
+      }      
+      //DBGlobal=db;  
+      db.transaction(firebird.ISOLATION_READ_COMMITED, 
+          function(err, transaction) {          
+            //TransactionGlobal=transaction  
+            if (err) {
+              reject(err)
+            return
+            }
+            resolve( { transaction, db })
+          })
+      }) //firebirdpool
+    })  //promisse
+  } //function
+  
+const CommitTransaction = (transaction, db) => {
+    return new Promise((resolve, reject) => {  
+      transaction.commitRetaining(function(err) {
+        if (err){
+            transaction.rollback()
+            reject(err)
+            return
+        }
+        else {
+          db.detach()
+          resolve(true)
+        }
+      })//transaction  
+    })//promisse
+  }    
+  
+const RollbackTransaction = (transaction, db) => {
+    return new Promise((resolve, reject) => {  
+      try{
+        transaction.rollback()
+        db.detach()
+        resolve(true)
+      }
+      catch(err){
+        reject(err)
+      }  
+    })//promisse
+  }
+      
+const QueryExecTransaction = (transaction, sql, arrayparams = []) => {
+  return new Promise((resolve, reject) => {  
+    transaction.query(sql,arrayparams,function(err, result) {    
+      if (err) {
+        console.log('erro in executing the query')
+        transaction.rollback()
+        reject(err)
+        return
+        }
+      resolve(result)
+      return
+     })  //query            
+    })//promisse
+  }
+  
+async function testa(){
+  const {transaction, db } = await NewTransaction()    
+    let psql='SELECT * FROM SALES'
+    try{    
+      await QueryExecTransaction(transaction, psql)
+      // await CommitTransaction(transaction,db);
+    }
+    catch(e){
+      console.log('Erro on SQL process')
+      console.log(e)
+      await RollbackTransaction(transaction, db)        
+    }
+    console.log('finished')
+  }
+
+testa()
 
 </script>
 
