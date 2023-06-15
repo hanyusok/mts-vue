aba05e22226c3ccf85905cd04295089900699b3b
hanyusok <hanyusok@gmail.com>
2023-06-15 18:10:04 +0900

    node firebird

--- src/database.js
+++ src/database.js
@@ -1,9 +1,9 @@
-//import firebird from 'node-firebird'
+import firebird from 'node-firebird'
 
 const dbOptions = {
     host: 'localhost',
     port: 3050,
-    database: '../../SALESDB.FDB',
+    database: './SALESDB.FDB',
     user: 'sysdba',
     password: 'masterkey',
     lowercase_keys: false, // set to true to lowercase keys
@@ -14,4 +14,75 @@ const dbOptions = {
     blobAsText: false // set to true to get blob as text, only affects blob subtype 1
   }
 
-  export { dbOptions }
+const firebirdPool = firebird.pool(5, dbOptions)
+  
+const  NewTransaction = () => {
+  return new Promise((resolve, reject) => {      
+    firebirdPool.get((err, db) => {
+      if (err) { 
+        reject(err)
+        return
+      }      
+      //DBGlobal=db;  
+      db.transaction(firebird.ISOLATION_READ_COMMITED, 
+          function(err, transaction) {          
+            //TransactionGlobal=transaction  
+            if (err) {
+              reject(err)
+            return
+            }
+            resolve( { transaction, db })
+          })
+      }) //firebirdpool
+    })  //promisse
+  } //function
+  
+const CommitTransaction = (transaction, db) => {
+    return new Promise((resolve, reject) => {  
+      transaction.commitRetaining(function(err) {
+        if (err){
+            transaction.rollback()
+            reject(err)
+            return
+        }
+        else {
+          db.detach()
+          resolve(true)
+        }
+      })//transaction  
+    })//promisse
+  }    
+  
+const RollbackTransaction = (transaction, db) => {
+    return new Promise((resolve, reject) => {  
+      try{
+        transaction.rollback()
+        db.detach()
+        resolve(true)
+      }
+      catch(err){
+        reject(err)
+      }  
+    })//promisse
+  }
+      
+const QueryExecTransaction = (transaction, sql, arrayparams = []) => {
+  return new Promise((resolve, reject) => {  
+    transaction.query(sql,arrayparams,function(err, result) {    
+      if (err) {
+        console.log('erro in executing the query')
+        transaction.rollback()
+        reject(err)
+        return
+        }
+      resolve(result)
+      return
+     })  //query            
+    })//promisse
+  }
+  
+export { dbOptions, 
+    firebirdPool, 
+    NewTransaction, 
+    RollbackTransaction,
+    QueryExecTransaction }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,115 +1,25 @@
 <script setup>
-import { dbOptions } from '../database'
-import firebird from 'node-firebird'
+import { setMapStoreSuffix } from 'pinia';
+import { dbOptions, firebirdPool, NewTransaction, RollbackTransaction,
+    QueryExecTransaction } from '../database'
 
 console.log('is starting')
 
-/* const tryConnect = () => {
-  firebird.attach(dbOptions, (err, db) => {
-    if (err)
-      throw err
-
-    // db = DATABASE
-    db.query('SELECT * FROM PERSON', (err, result) => {
-      // IMPORTANT: close the connection
-      // console.log(result)
-
-      db.detach()
-
-      if (err) {
-        console.log(err)
-      } else {
-        console.log(result)
-      }
-    })
-
-  })
-} */
-
-
-const firebirdPool = firebird.pool(5, dbOptions);
-  
-const  NewTransaction = () => {
-  return new Promise((resolve, reject) => {      
-    firebirdPool.get((err, db) => {
-      if (err) { 
-        reject(err)
-        return
-      }      
-      //DBGlobal=db;  
-      db.transaction(firebird.ISOLATION_READ_COMMITED, 
-          function(err, transaction) {          
-            //TransactionGlobal=transaction  
-            if (err) {
-              reject(err)
-            return
-            }
-            resolve( { transaction, db })
-          })
-      }) //firebirdpool
-    })  //promisse
-  } //function
-  
-const CommitTransaction = (transaction, db) => {
-    return new Promise((resolve, reject) => {  
-      transaction.commitRetaining(function(err) {
-        if (err){
-            transaction.rollback()
-            reject(err)
-            return
-        }
-        else {
-          db.detach()
-          resolve(true)
-        }
-      })//transaction  
-    })//promisse
-  }    
-  
-const RollbackTransaction = (transaction, db) => {
-    return new Promise((resolve, reject) => {  
-      try{
-        transaction.rollback()
-        db.detach()
-        resolve(true)
-      }
-      catch(err){
-        reject(err)
-      }  
-    })//promisse
-  }
-      
-const QueryExecTransaction = (transaction, sql, arrayparams = []) => {
-  return new Promise((resolve, reject) => {  
-    transaction.query(sql,arrayparams,function(err, result) {    
-      if (err) {
-        console.log('erro in executing the query')
-        transaction.rollback()
-        reject(err)
-        return
-        }
-      resolve(result)
-      return
-     })  //query            
-    })//promisse
-  }
-  
-async function testa(){
+const testa = async() => {
   const {transaction, db } = await NewTransaction()    
     let psql='SELECT * FROM SALES'
     try{    
-      await QueryExecTransaction(transaction, psql)
-      // await CommitTransaction(transaction,db);
+      await QueryExecTransaction(transaction, psql)      
     }
     catch(e){
-      console.log('Erro on SQL process')
+      console.log('Error on SQL process')
       console.log(e)
       await RollbackTransaction(transaction, db)        
     }
     console.log('finished')
   }
 
-testa()
+
 
 </script>
 
